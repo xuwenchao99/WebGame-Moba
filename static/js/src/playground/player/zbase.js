89ba1fcede22ca364dbb7de4d1ceb60753a34838
徐文超 <2479157778@qq.com>
2022-01-12 23:13:31 +0800

    update check out work

--- static/js/src/playground/player/zbase.js
+++ static/js/src/playground/player/zbase.js
@@ -145,7 +145,7 @@ class Player extends AcGameObject {
         let fireball = new FireBall(this.playground, this, x, y, radius, vx, vy, color, speed, move_length, 0.01);
         this.fireballs.push(fireball);
 
-        this.fireball_coldtime = 0.1;
+        this.fireball_coldtime = 3;
 
         return fireball;
     }
